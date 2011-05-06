23dc5d1f4df83f50a4d5b81c7956c5e5ed069ffe
Marlon Pierce <mpierce@apache.org>
2011-05-06 21:49:09 +0000

    Adding a layout for the OpenID user.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rave/trunk@1100392 13f79535-47bb-0310-9956-ffa450edef68

--- rave-portal/src/main/resources/initial_data.sql
+++ rave-portal/src/main/resources/initial_data.sql
@@ -139,3 +139,23 @@ values (next value for region_widget_id_seq, @hamster_widget_id, @page_1_region_
 INSERT INTO region_widget(id, widget_id, region_id, render_position, collapsed)
 values (next value for region_widget_id_seq, @tabnews_widget_id, @page_1_region_2, 2, 'N');
 --- End canonical user_id_3 layout ---
+
+--- Layout for user_id_6 ---
+INSERT INTO page (id, name, owner_id, render_sequence, page_layout_id)
+values (set(@page_1_id, next value for page_id_seq), 'main', @user_id_6, 1, @two_col_id);
+
+INSERT INTO region(id, page_id)
+values (set(@page_1_region_1, next value for region_id_seq), @page_1_id);
+INSERT INTO region(id, page_id)
+values (set(@page_1_region_2, next value for region_id_seq), @page_1_id);
+
+INSERT INTO region_widget(id, widget_id, region_id, render_position, collapsed)
+values (next value for region_widget_id_seq, @wikipedia_widget_id, @page_1_region_1, 1, 'N');
+INSERT INTO region_widget(id, widget_id, region_id, render_position, collapsed)
+values (next value for region_widget_id_seq, @translate_widget_id, @page_1_region_1, 2, 'N');
+
+INSERT INTO region_widget(id, widget_id, region_id, render_position, collapsed)
+values (next value for region_widget_id_seq, @hamster_widget_id, @page_1_region_2, 1, 'N');
+INSERT INTO region_widget(id, widget_id, region_id, render_position, collapsed)
+values (next value for region_widget_id_seq, @tabnews_widget_id, @page_1_region_2, 2, 'N');
+--- End canonical user_id_6 layout ---
