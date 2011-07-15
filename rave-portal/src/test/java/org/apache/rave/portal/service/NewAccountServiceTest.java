f806f807e2a309767cd9b58822df009c07b7cc93
Marlon Pierce <mpierce@apache.org>
2011-07-15 22:12:19 +0000

    (RAVE-89) adding newaccountservice test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rave/trunk@1147333 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rave-portal/src/test/java/org/apache/rave/portal/service/NewAccountServiceTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.rave.portal.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.rave.portal.model.User;
+import org.apache.rave.portal.service.impl.DefaultNewAccountService;
+
+public class NewAccountServiceTest {
+
+	 @Test
+	 public void createNewAccountTest(){
+		  //TODO implement test
+	 }
+
+	 @Test
+	 public void failedAccountCreationTest() {
+		  //TODO implement test to check failure conditions such as using already
+		  //used user name.
+	 }
+}
