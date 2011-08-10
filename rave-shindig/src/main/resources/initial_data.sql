86ea6707227520d854c2b437abb6921d4a07e568
Matthew B. Franklin <mfranklin@apache.org>
2011-08-10 00:33:52 +0000

    Moved rave-shindig test data to runtime initial data (Supports RAVE-122)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rave/trunk@1155982 13f79535-47bb-0310-9956-ffa450edef68

--- rave-shindig/src/main/resources/initial_data.sql
+++ rave-shindig/src/main/resources/initial_data.sql
@@ -14,3 +14,29 @@
  -- KIND, either express or implied.  See the License for the
  -- specific language governing permissions and limitations
  -- under the License.
+
+INSERT INTO person(id, username, display_name, gender, status, profile_url, build, eye_color, hair_color,family_name, given_name )
+VALUES (set(@person_id_1, next value for person_id_seq), 'canonical', 'Canonical User', 'male', 'I am alive', 'http://rave.rocks.org/profile', 'skinny', 'orange', 'blue', 'User', 'Canonical');
+
+INSERT INTO person(id, username, display_name)
+VALUES (set(@person_id_2, next value for person_id_seq), 'john.doe', 'John Doe');
+
+
+INSERT INTO person(id, username, display_name)
+VALUES (set(@person_id_3, next value for person_id_seq), 'jane.doe', 'Jane Doe');
+
+
+INSERT INTO person(id, username, display_name)
+VALUES (set(@person_id_4, next value for person_id_seq), 'george.doe', 'George Doe');
+
+INSERT INTO person(id, username, display_name)
+VALUES (set(@person_id_5, next value for person_id_seq), 'mario.rossi', 'Mario Rossi');
+
+INSERT INTO person_association(id, follower_id, followed_id)
+VALUES (next value for person_association_id_seq, @person_id_1, @person_id_2);
+
+INSERT INTO person_association(id, follower_id, followed_id)
+VALUES (next value for person_association_id_seq, @person_id_1, @person_id_3);
+
+INSERT INTO person_association(id, follower_id, followed_id)
+VALUES (next value for person_association_id_seq, @person_id_2, @person_id_4);
