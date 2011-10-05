3c57a40f573fa0c86b0a4afd800daf06f6440a6d
Jasha Joachimsthal <jasha@apache.org>
2011-10-05 11:58:09 +0000

    RAVE-232 add missing license headers to new classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rave/trunk@1179180 13f79535-47bb-0310-9956-ffa450edef68

--- rave-components/rave-core/src/test/java/org/apache/rave/portal/model/AuthorityTest.java
+++ rave-components/rave-core/src/test/java/org/apache/rave/portal/model/AuthorityTest.java
@@ -17,21 +17,25 @@
  * under the License.
  */
 
-package org.apache.rave.portal.repository;
+package org.apache.rave.portal.model;
 
-import org.apache.rave.persistence.Repository;
-import org.apache.rave.portal.model.Authority;
+import org.junit.Test;
+import org.springframework.security.core.GrantedAuthority;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
- * Repository interface for {@link org.apache.rave.portal.model.Authority}
+ * Test for {@link Authority}
  */
-public interface AuthorityRepository extends Repository<Authority> {
+public class AuthorityTest {
 
-    /**
-     * Finds the {@link Authority} by its name
-     *
-     * @param authorityName (unique) name of the Authority
-     * @return Authority if it can be found, otherwise {@literal null}
-     */
-    Authority getByAuthority(String authorityName);
+    @Test
+    public void testAuthority() throws Exception {
+        GrantedAuthority grantedAuthority = new Authority();
+        ((Authority) grantedAuthority).setAuthority("user");
+        assertEquals("user", grantedAuthority.getAuthority());
+
+        GrantedAuthority grantedAuthority2 = new Authority("admin");
+        assertEquals("admin", grantedAuthority2.getAuthority());
+    }
 }
