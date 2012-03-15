6011eb6abf164575445ab00f6a5c332b07c3f5b2
Anthony Carlucci <carlucci@apache.org>
2012-03-15 18:25:01 +0000

    RAVE-512: OpenSocial gadgets on non-active Person Profile tabs are not autosizing properly
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rave/trunk@1301128 13f79535-47bb-0310-9956-ffa450edef68

--- rave-portal-resources/src/main/webapp/script/rave_person_profile.js
+++ rave-portal-resources/src/main/webapp/script/rave_person_profile.js
@@ -19,10 +19,38 @@
 
 var rave = rave || {};
 rave.personprofile = rave.personprofile || (function() {
-	function init() {
-        // setup the sub page tabs
-        $("#personProfileSubPages").tabs();
+    // map of [subpage id, boolean] tracking whether or not a sub page has been viewed at least once
+    var subPagesViewedStatus = {};
 
+    function initSubPages() {
+        // setup the sub page tabs
+        $("#personProfileSubPages").tabs({
+                select: function(event, ui) {
+                    // refresh the widgets on the sub page when selected to ensure proper sizing
+                    var subPageId = ui.panel.id;
+                    if (subPagesViewedStatus[subPageId] == false) {
+                        $("#" + subPageId + " .widget-wrapper").each(function(){
+                            var regionWidget = rave.getRegionWidgetById(rave.getObjectIdFromDomId(this.id));
+                            regionWidget.restore();
+                        });
+                        // mark that this sub page has been viewed at least once and there is no need to refresh
+                        // the widgets in future views
+                        subPagesViewedStatus[subPageId] = true;
+                    }
+                }
+            }
+        );
+
+        // build the subPageViewedStatus map to track if a given sub page has been viewed yet to determine if we need
+        // to refresh the widgets upon first viewing to ensure they are sized properly.  Set the default active tab to
+        // true since it will be rendered and sized properly as part of the initial page load
+        var activeSubPageId = $("#personProfileSubPages .ui-tabs-panel:not(.ui-tabs-hide)")[0].id;
+        $("#personProfileSubPages .ui-tabs-panel").each(function(){
+            subPagesViewedStatus[this.id] = (this.id == activeSubPageId);
+        });
+    }
+
+    function initButtons() {
         // setup the edit button if it exists
         var $editButton = $("#profileEdit");
         if ($editButton) {
@@ -38,6 +66,11 @@ rave.personprofile = rave.personprofile || (function() {
                 rave.api.handler.userProfileEditHandler(false);
             });
         }
+    }
+
+	function init() {
+        initSubPages();
+        initButtons();
     }
 	
 	return {
