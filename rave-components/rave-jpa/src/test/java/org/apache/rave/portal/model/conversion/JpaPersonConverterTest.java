5ec4d55b322d42f3683b0a29c3a977e18a2d4393
Matthew B. Franklin <mfranklin@apache.org>
2012-06-19 19:37:11 +0000

    Updated Person conversion RAVE-652
    git-svn-id: https://svn.apache.org/repos/asf/rave/branches/model_interfaces@1351814 13f79535-47bb-0310-9956-ffa450edef68

--- rave-components/rave-jpa/src/test/java/org/apache/rave/portal/model/conversion/JpaPersonConverterTest.java
+++ rave-components/rave-jpa/src/test/java/org/apache/rave/portal/model/conversion/JpaPersonConverterTest.java
@@ -1,7 +1,6 @@
 package org.apache.rave.portal.model.conversion;
 
-import org.apache.rave.portal.model.JpaPerson;
-import org.apache.rave.portal.model.Person;
+import org.apache.rave.portal.model.*;
 import org.apache.rave.portal.model.impl.PersonImpl;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
@@ -41,12 +42,41 @@ public class JpaPersonConverterTest {
     @Test
     public void convertValid() {
         Person template = new PersonImpl();
+        template.setUsername("TEST_A");
+        template.setEmail("TEST_B");
+        template.setDisplayName("TEST_C");
+        template.setAdditionalName("TEST_D");
+        template.setFamilyName("TEST_E");
+        template.setGivenName("TEST_F");
+        template.setHonorificPrefix("TEST_G");
+        template.setHonorificSuffix("TEST_H");
+        template.setPreferredName("TEST_I");
+        template.setAboutMe("TEST_J");
+        template.setStatus("TEST_K");
+        template.setAddresses(new ArrayList<Address>());
+        template.setOrganizations(new ArrayList<Organization>());
+        template.setProperties(new ArrayList<PersonProperty>());
+        template.setFriends(new ArrayList<Person>());
 
         JpaPerson jpaTemplate = converter.convert(template);
 
         assertThat(jpaTemplate, is(not(sameInstance(template))));
         assertThat(jpaTemplate, is(instanceOf(JpaPerson.class)));
-        //TODO: Add coverage for all methods
+        assertThat(jpaTemplate.getUsername(), is(equalTo(template.getUsername())));
+        assertThat(jpaTemplate.getEmail(), is(equalTo(template.getEmail())));
+        assertThat(jpaTemplate.getDisplayName(), is(equalTo(template.getDisplayName())));
+        assertThat(jpaTemplate.getUsername(), is(equalTo(template.getUsername())));
+        assertThat(jpaTemplate.getFamilyName(), is(equalTo(template.getFamilyName())));
+        assertThat(jpaTemplate.getGivenName(), is(equalTo(template.getGivenName())));
+        assertThat(jpaTemplate.getHonorificPrefix(), is(equalTo(template.getHonorificPrefix())));
+        assertThat(jpaTemplate.getHonorificSuffix(), is(equalTo(template.getHonorificSuffix())));
+        assertThat(jpaTemplate.getPreferredName(), is(equalTo(template.getPreferredName())));
+        assertThat(jpaTemplate.getAboutMe(), is(equalTo(template.getAboutMe())));
+        assertThat(jpaTemplate.getStatus(), is(equalTo(template.getStatus())));
+        assertThat(jpaTemplate.getAddresses(), is(equalTo(template.getAddresses())));
+        assertThat(jpaTemplate.getOrganizations(), is(equalTo(template.getOrganizations())));
+        assertThat(jpaTemplate.getProperties(), is(equalTo(template.getProperties())));
+        assertThat(jpaTemplate.getFriends(), is(equalTo(template.getFriends())));
     }
 
 }
