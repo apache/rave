6aa89c3057db7c9688a208fe83d4f6e4b1794959
Matthew B. Franklin <mfranklin@apache.org>
2012-06-19 19:20:51 +0000

    Updated WidgetRating conversion RAVE-652
    git-svn-id: https://svn.apache.org/repos/asf/rave/branches/model_interfaces@1351805 13f79535-47bb-0310-9956-ffa450edef68

--- rave-components/rave-jpa/src/main/java/org/apache/rave/portal/model/conversion/JpaWidgetRatingConverter.java
+++ rave-components/rave-jpa/src/main/java/org/apache/rave/portal/model/conversion/JpaWidgetRatingConverter.java
@@ -5,17 +5,11 @@ import org.apache.rave.portal.model.JpaWidgetRating;
 import org.apache.rave.portal.model.WidgetRating;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 /**
  * Converts a WidgetRating to a JpaWidgetRating
  */
 @Component
 public class JpaWidgetRatingConverter implements ModelConverter<WidgetRating, JpaWidgetRating> {
-
-    @PersistenceContext
-    private EntityManager manager;
 
     @Override
     public Class<WidgetRating> getSourceType() {
@@ -29,18 +23,15 @@ public class JpaWidgetRatingConverter implements ModelConverter<WidgetRating, Jp
 
     private JpaWidgetRating createEntity(WidgetRating source) {
         JpaWidgetRating converted = null;
-        if (source != null) {
-            converted = manager.find(JpaWidgetRating.class, source.getId());
-
-            if (converted == null) {
-                converted = new JpaWidgetRating();
-            }
+        if(source != null) {
+            converted = new JpaWidgetRating();
             updateProperties(source, converted);
         }
         return converted;
     }
 
     private void updateProperties(WidgetRating source, JpaWidgetRating converted) {
+        converted.setId(source.getId());
         converted.setScore(source.getScore());
         converted.setUserId(source.getUserId());
         converted.setWidgetId(source.getWidgetId());
--- rave-components/rave-jpa/src/test/java/org/apache/rave/portal/model/conversion/JpaWidgetRatingConverterTest.java
+++ rave-components/rave-jpa/src/test/java/org/apache/rave/portal/model/conversion/JpaWidgetRatingConverterTest.java
@@ -41,12 +41,17 @@ public class JpaWidgetRatingConverterTest {
     @Test
     public void convertValid() {
         WidgetRating template = new WidgetRatingImpl();
-
+        template.setScore(1);
+        template.setUserId(42L);
+        template.setWidgetId(24L);
+        
         JpaWidgetRating jpaTemplate = converter.convert(template);
 
         assertThat(jpaTemplate, is(not(sameInstance(template))));
         assertThat(jpaTemplate, is(instanceOf(JpaWidgetRating.class)));
-        //TODO: Add coverage for all methods
+        assertThat(jpaTemplate.getScore(), is(equalTo(template.getScore())));
+        assertThat(jpaTemplate.getUserId(), is(equalTo(template.getUserId())));
+        assertThat(jpaTemplate.getWidgetId(), is(equalTo(template.getWidgetId())));
     }
 
 }
