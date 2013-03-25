98acafe46cce564f705548985458a12019391eba
Matthew B. Franklin <mfranklin@apache.org>
2013-03-25 22:08:43 +0000

    Added required initialization parameters back into hub RAVE-920
    git-svn-id: https://svn.apache.org/repos/asf/rave/trunk@1460915 13f79535-47bb-0310-9956-ffa450edef68

--- rave-portal-resources/src/main/webapp/static/script/core/rave_core.js
+++ rave-portal-resources/src/main/webapp/static/script/core/rave_core.js
@@ -129,7 +129,20 @@ rave = (function () {
                 throw new Error("No implementation of OpenAjax found.  " +
                     "Please ensure that an implementation has been included in the page.");
             }
-            openAjaxHub = new OpenAjax.hub.ManagedHub();
+            openAjaxHub = new OpenAjax.hub.ManagedHub({
+                    onSubscribe:function (topic, container) {
+                        rave.log((container == null ? "Container" : container.getClientID()) + " subscribes to this topic '" + topic + "'");
+                        return true;
+                    },
+                    onUnsubscribe:function (topic, container) {
+                        rave.log((container == null ? "Container" : container.getClientID()) + " unsubscribes from this topic '" + topic + "'");
+                        return true;
+                    },
+                    onPublish:function (topic, data, pcont, scont) {
+                        rave.log((pcont == null ? "Container" : pcont.getClientID()) + " publishes '" + data + "' to topic '" + topic + "' subscribed by " + (scont == null ? "Container" : scont.getClientID()));
+                        return true;
+                    }
+            });
         }
         return openAjaxHub;
     }
