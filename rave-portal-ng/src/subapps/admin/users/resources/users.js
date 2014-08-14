0374f805a230a3b74c2e9002de8e22aa1e654a91
Jmeas <jellyes2@gmail.com>
2014-08-14 13:34:14 -0400

    Adds default Authorization header.

--- rave-portal-ng/src/subapps/admin/users/resources/users.js
+++ rave-portal-ng/src/subapps/admin/users/resources/users.js
@@ -10,26 +10,8 @@ define(function(require) {
   var URL = '/api/v1/users';
 
   // Return the categories resource
-  return ['$resource', 'authToken',
-  function($resource, authToken) {
-
-    var authHeader = {};
-
-    // We return a factory method from this due to logging in and out
-    // requiring a dynamic header
-    return function() {
-
-      // Dynamically set the authorization header, based on our current authToken
-      authHeader.Authorization = 'Basic ' + authToken.get();
-
-      // Return the resource.
-      return $resource(URL, {}, {
-        query: {
-          method: 'GET',
-          isArray: true,
-          headers: authHeader
-        }
-      });
-    };
+  return ['$resource',
+  function($resource) {
+    return $resource(URL);
   }];
 });
